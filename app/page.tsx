41b40627d85f99aa765a847d69e6d33928e6785a
Natalie Davis <natalienetworks@gmail.com>
2024-11-26 19:28:17 -0600

    testing method broke deploy

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { background } from '@/utils/todo';
 export default async function page() {
   return (
     <div className={background}>
-      <Feed />
+      <Feed did='did:plc:qzkrgc4ahglknwb7ymee4a6w' feedName='aaafstml2groe' />
     </div>
   );
 }
