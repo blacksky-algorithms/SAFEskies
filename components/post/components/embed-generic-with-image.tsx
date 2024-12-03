fdb268d7ccd41ecc84866582196f9997f8e91a7b
Natalie Davis <natalienetworks@gmail.com>
2024-12-02 21:43:29 -0600

    not implemented modal

--- components/post/components/embed-generic-with-image.tsx
+++ components/post/components/embed-generic-with-image.tsx
@@ -1,4 +1,9 @@
+import { Button } from '@/components/button';
+import { NotImplementedModal } from '@/components/modals/not-implemented-modal';
+import { useModal } from '@/contexts/modal-context';
+import { MODAL_INSTANCE_IDS } from '@/enums/modals';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export const GenericWithImageEmbed = ({
   title,
@@ -13,28 +18,51 @@ export const GenericWithImageEmbed = ({
   image?: string;
   description?: string;
 }) => {
-  // TODO: This renders a link to a feed or list, add a not yet supported modal that informs user will be sent to bluesky
+  const { openModalInstance, closeModalInstance } = useModal();
+  const [confirmedHref, setConfirmedHref] = useState<string | null>(null);
+  const onCloseModal = () => {
+    if (confirmedHref) {
+      window.location.href = href;
+      closeModalInstance(MODAL_INSTANCE_IDS.NOT_IMPLEMENTED);
+    }
+
+    closeModalInstance(MODAL_INSTANCE_IDS.NOT_IMPLEMENTED);
+  };
+  // renders a link to a feed or list
   return (
-    <Link
-      href={href}
-      className='w-full rounded-lg border py-2 px-3 flex flex-col gap-2'
-    >
-      <div className='flex gap-2.5 items-center'>
-        {image ? (
-          <img
-            src={image}
-            alt={title}
-            className='w-8 h-8 rounded-lg  shrink-0'
-          />
-        ) : (
-          <div className='w-8 h-8 rounded-lg bg-brand shrink-0' />
-        )}
-        <div className='flex-1'>
-          <p className='font-bold text-sm'>{title}</p>
-          <p className='text-textLight text-sm'>{subtitle}</p>
+    <>
+      <Link
+        onClick={() => openModalInstance(MODAL_INSTANCE_IDS.NOT_IMPLEMENTED)}
+        className='w-full rounded-lg border py-2 px-3 flex flex-col gap-2'
+        href={confirmedHref ? href : '#'}
+      >
+        <div className='flex gap-2.5 items-center'>
+          {image ? (
+            <img
+              src={image}
+              alt={title}
+              className='w-8 h-8 rounded-lg  shrink-0'
+            />
+          ) : (
+            <div className='w-8 h-8 rounded-lg bg-brand shrink-0' />
+          )}
+          <div className='flex-1'>
+            <p className='font-bold text-sm'>{title}</p>
+            <p className='text-textLight text-sm'>{subtitle}</p>
+          </div>
         </div>
-      </div>
-      {description && <p className='text-textLight text-sm'>{description}</p>}
-    </Link>
+        {description && <p className='text-textLight text-sm'>{description}</p>}
+      </Link>
+      <NotImplementedModal>
+        <p className='text-center'>
+          This feature has not yet been implemented on OnlyFeeds, if you
+          confirmedHref you will be going to the Blue Sky App
+        </p>
+        <div>
+          <Button onClick={() => setConfirmedHref(href)}>Proceed</Button>
+          <Button onClick={onCloseModal}>Cancel</Button>
+        </div>
+      </NotImplementedModal>
+    </>
   );
 };
