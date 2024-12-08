896eb475b25b58e5d1daec96edfdce75c08bc338
Natalie Davis <natalienetworks@gmail.com>
2024-12-08 13:39:45 -0600

    removed page

--- app/oauth/callback/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function page() {
-  return (
-    <section className='flex items-center justify-center w-full h-full px-4'>
-      <h1>Oauth is authin</h1>
-    </section>
-  );
-}
