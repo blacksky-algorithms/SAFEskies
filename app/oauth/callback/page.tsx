f3fadbda325676ac9d3754362c5baf9d87f732b0
Natalie Davis <natalienetworks@gmail.com>
2024-12-08 13:37:42 -0600

    added a page to oauth/callback

---
+++ app/oauth/callback/page.tsx
@@ -0,0 +1,7 @@
+export default async function page() {
+  return (
+    <section className='flex items-center justify-center w-full h-full px-4'>
+      <h1>Oauth is authin</h1>
+    </section>
+  );
+}
