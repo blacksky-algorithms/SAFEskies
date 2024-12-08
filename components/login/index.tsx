4c0090af9132dbf29130988eb5b1ef089c370687
Natalie Davis <natalienetworks@gmail.com>
2024-12-08 15:49:22 -0600

    remove classNames

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -44,7 +44,6 @@ export const Login = () => {
               name='handle'
               type='text'
               placeholder='handle.bsky.social'
-              className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
               value={handle}
               onChange={(event) => setHandle(event.target.value)}
             />
