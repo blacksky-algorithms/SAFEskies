9be03d0c3fc115e8ffc2b891029fea17ee837280
Natalie Davis <natalienetworks@gmail.com>
2024-12-08 09:15:32 -0600

    first pass auth

---
+++ app/private/page.tsx
@@ -0,0 +1,26 @@
+import LoginButton from '@/components/button/login-button';
+import LogoutButton from '@/components/button/logout-button';
+import getSession from '@/repos/iron';
+
+export default async function Page() {
+  const session = await getSession();
+  console.log({ session });
+
+  if (!session.user) {
+    return (
+      <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
+        <span>
+          You need to be logged in to view this page. <LoginButton />
+        </span>
+      </main>
+    );
+  }
+
+  return (
+    <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
+      <span>
+        Private page, welcome {session.user.name}! <LogoutButton />
+      </span>
+    </main>
+  );
+}
--- contexts/index.tsx
+++ contexts/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { ModalProvider } from './modal-context';
+import { AuthProvider } from './auth-context';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <ModalProvider>{children}</ModalProvider>;
+  return (
+    <AuthProvider>
+      <ModalProvider>{children}</ModalProvider>
+    </AuthProvider>
+  );
 };
