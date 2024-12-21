cc240c3f14350b655a90d31588b23f9e93b6f1f3
Natalie Davis <natalienetworks@gmail.com>
2024-12-21 13:54:59 -0600

    side drawer closes as expected after clicking a link

--- components/side-drawer/side-drawer-content/index.tsx
+++ components/side-drawer/side-drawer-content/index.tsx
@@ -6,19 +6,32 @@ import { User } from '@/types/user';
 import { UserDrawerContent } from './user-drawer-content';
 import { ModSideDrawerContent } from './mod-side-drawer-content';
 import { AdminSideDrawerContent } from './admin-side-drawer-content';
+import { useModal } from '@/contexts/modal-context';
+import { MODAL_INSTANCE_IDS } from '@/enums/modals';
+import { useRouter } from 'next/navigation';
 
 interface Props {
   user: User | null;
 }
 
 export const SideDrawerContent = ({ user }: Props) => {
+  const router = useRouter();
+  const { closeModalInstance } = useModal();
   if (!user) {
     return null;
   }
 
+  const handleLinkClick =
+    (href: string) => (event: React.MouseEvent<HTMLAnchorElement>) => {
+      event.preventDefault();
+      closeModalInstance(MODAL_INSTANCE_IDS.SIDE_DRAWER);
+      router.push(href);
+    };
   const SIDE_DRAWER_CONTENT = {
-    admin: <AdminSideDrawerContent user={user} />,
-    mod: <ModSideDrawerContent user={user} />,
+    admin: (
+      <AdminSideDrawerContent user={user} handleLinkClick={handleLinkClick} />
+    ),
+    mod: <ModSideDrawerContent user={user} handleLinkClick={handleLinkClick} />,
     user: <UserDrawerContent user={user} />,
   };
 
