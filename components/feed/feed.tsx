232a28687d09fdab059f1f54fc49342b9871273c
Natalie Davis <natalienetworks@gmail.com>
2025-01-27 18:59:27 -0600

    abstracted moderation functionality

--- components/feed/feed.tsx
+++ components/feed/feed.tsx
@@ -14,26 +14,13 @@ import { HydratedPostModal } from '../modals/hydrated-post';
 import { VisualIntent } from '@/enums/styles';
 import { ModMenuModal } from '../modals/mod-menu';
 import { ReportPostModal } from '../modals/report-post';
-import { ReportOption } from '@/lib/types/moderation';
-import { useToast } from '@/contexts/toast-context';
-import {
-  MODERATION_SERVICES,
-  ModerationService,
-} from '@/lib/constants/moderation';
-import { ReasonType } from '@atproto/api/dist/client/types/com/atproto/moderation/defs';
 import { usePullToRefresh } from '@/hooks/usePullToRefresh';
+import { useModeration } from '@/hooks/useModeration';
 
 interface FeedProps {
   uri: string;
   onRefreshComplete?: () => void;
   feedName: string | undefined;
-}
-
-interface ReportDataState {
-  post: PostView | null;
-  reason: ReportOption | null;
-  toServices: typeof MODERATION_SERVICES;
-  moderatedPostUri: string | null;
 }
 
 export const Feed = ({ uri, onRefreshComplete, feedName }: FeedProps) => {
@@ -52,15 +39,18 @@ export const Feed = ({ uri, onRefreshComplete, feedName }: FeedProps) => {
     });
 
   const { openModalInstance, closeModalInstance } = useModal();
-  const { toast } = useToast();
   const [viewedPostUri, setViewedPostUri] = useState<string | null>(null);
-  const [isReportSubmitting, setIsReportSubmitting] = useState(false);
-  const [reportData, setReportData] = useState<ReportDataState>({
-    post: null,
-    reason: null,
-    toServices: [MODERATION_SERVICES[0]],
-    moderatedPostUri: null,
-  });
+
+  const {
+    reportData,
+    isReportSubmitting,
+    handleModAction,
+    handleSelectReportReason,
+    handleReportPost,
+    handleReportToChange,
+    isModServiceChecked,
+    onClose,
+  } = useModeration({ feedUri: uri, feedName, feed });
 
   useEffect(() => {
     if (error) {
@@ -88,148 +78,20 @@ export const Feed = ({ uri, onRefreshComplete, feedName }: FeedProps) => {
     openModalInstance(MODAL_INSTANCE_IDS.HYDRATED_POST, true);
   };
 
-  const handleModAction = (post: PostView) => {
-    setReportData((prev) => ({
-      ...prev,
-      moderatedPostUri: post.uri,
-    }));
-    openModalInstance(MODAL_INSTANCE_IDS.MOD_MENU, true);
-  };
-
   const handleErrorModalClose = () => {
     closeModalInstance(MODAL_INSTANCE_IDS.GENERIC_ERROR);
     refreshFeed();
   };
 
-  const handleSelectReportReason = (reason: ReportOption) => {
-    setReportData((prev) => ({
-      ...prev!,
-      reason,
-    }));
-    openModalInstance(MODAL_INSTANCE_IDS.REPORT_POST, true);
-  };
-
-  const reportModerationEvent = async (payload: {
-    targetedPostUri: string;
-    reason: ReasonType;
-    toServices: { label: string; value: string }[];
-    targetedUserDid: string;
-    feedUri: string;
-    feedName: string | undefined;
-  }) => {
-    try {
-      await fetch('/api/permissions/mod-event', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(payload),
-      });
-    } catch (error) {
-      console.error('Error logging moderation event:', error);
-    }
-  };
-
-  const handleReportPost = async () => {
-    if (
-      !reportData.moderatedPostUri ||
-      !reportData.reason ||
-      feed.length === 0
-    ) {
-      toast({
-        title: 'Error',
-        message: 'No Post Selected.',
-        intent: VisualIntent.Error,
-      });
-      return;
-    }
-
-    try {
-      const postToModerate = feed.find(
-        (post) => post.post.uri === reportData.moderatedPostUri
-      );
-      if (!postToModerate) {
-        toast({
-          title: 'Error',
-          message: 'No Post Selected.',
-          intent: VisualIntent.Error,
-        });
-        return;
-      }
-      setIsReportSubmitting(true);
-
-      const payload = {
-        targetedPostUri: reportData.moderatedPostUri,
-        reason: reportData.reason.reason,
-        toServices: reportData.toServices,
-        targetedUserDid: postToModerate.post.author.did,
-        feedUri: uri,
-        feedName: feedName || 'Unnamed Feed',
-      };
-
-      await reportModerationEvent(payload);
-
-      closeModalInstance(MODAL_INSTANCE_IDS.REPORT_POST);
-      closeModalInstance(MODAL_INSTANCE_IDS.MOD_MENU);
-
-      setReportData((prev) => ({
-        ...prev,
-        moderatedPostUri: null,
-        toServices: [MODERATION_SERVICES[0]],
-      }));
-      toast({
-        title: 'Success',
-        message: 'Post reported successfully',
-        intent: VisualIntent.Success,
-      });
-    } catch (error) {
-      console.error(error);
-      toast({
-        title: 'Error',
-        message: 'Unable to report post. Please try again later.',
-        intent: VisualIntent.Error,
-      });
-      setIsReportSubmitting(false);
-    } finally {
-      setIsReportSubmitting(false);
-    }
-  };
-
-  const isModServiceChecked = (data: ModerationService) =>
-    reportData.toServices.some((item) => item.value === data.value);
-
-  const handleReportToChange = (updatedReportToData: ModerationService) => {
-    if (isModServiceChecked(updatedReportToData)) {
-      setReportData((prev) => ({
-        ...prev,
-        toServices: prev.toServices.filter(
-          (item) => item.value !== updatedReportToData.value
-        ),
-      }));
-    } else {
-      setReportData((prev) => ({
-        ...prev,
-        toServices: [...prev.toServices, updatedReportToData],
-      }));
-    }
-  };
-
-  const onClose = () =>
-    setReportData((prev) => ({
-      ...prev,
-      moderatedPostUri: null,
-      toServices: [MODERATION_SERVICES[0]],
-    }));
-
   return (
     <>
       <div className='max-h-page'>
-        <section className='flex flex-col items-center mx-auto tablet:px-10 '>
+        <section className='flex flex-col items-center mx-auto tablet:px-10'>
           <div
             ref={containerRef}
             onTouchStart={handleTouchStart}
             onTouchMove={handleTouchMove}
-            className='overflow-y-auto h-screen flex flex-col items-center '
+            className='overflow-y-auto h-screen flex flex-col items-center'
           >
             <LiveRegion>
               {isRefreshing && <span>Refreshing...</span>}
---
+++ hooks/useModeration.tsx
@@ -0,0 +1,183 @@
+import { useState } from 'react';
+import {
+  FeedViewPost,
+  PostView,
+} from '@atproto/api/dist/client/types/app/bsky/feed/defs';
+import { ReportOption } from '@/lib/types/moderation';
+import { MODAL_INSTANCE_IDS } from '@/enums/modals';
+import { useModal } from '@/contexts/modal-context';
+import { useToast } from '@/contexts/toast-context';
+import {
+  MODERATION_SERVICES,
+  ModerationService,
+} from '@/lib/constants/moderation';
+import { ReasonType } from '@atproto/api/dist/client/types/com/atproto/moderation/defs';
+import { VisualIntent } from '@/enums/styles';
+
+interface ReportDataState {
+  post: PostView | null;
+  reason: ReportOption | null;
+  toServices: typeof MODERATION_SERVICES;
+  moderatedPostUri: string | null;
+}
+
+interface UseModerationOptions {
+  feedUri: string;
+  feedName?: string;
+  feed: FeedViewPost[];
+}
+
+export function useModeration({
+  feedUri,
+  feedName,
+  feed,
+}: UseModerationOptions) {
+  const { openModalInstance, closeModalInstance } = useModal();
+  const { toast } = useToast();
+  const [isReportSubmitting, setIsReportSubmitting] = useState(false);
+  const [reportData, setReportData] = useState<ReportDataState>({
+    post: null,
+    reason: null,
+    toServices: [MODERATION_SERVICES[0]],
+    moderatedPostUri: null,
+  });
+
+  const handleModAction = (post: PostView) => {
+    setReportData((prev) => ({
+      ...prev,
+      moderatedPostUri: post.uri,
+    }));
+    openModalInstance(MODAL_INSTANCE_IDS.MOD_MENU, true);
+  };
+
+  const handleSelectReportReason = (reason: ReportOption) => {
+    setReportData((prev) => ({
+      ...prev!,
+      reason,
+    }));
+    openModalInstance(MODAL_INSTANCE_IDS.REPORT_POST, true);
+  };
+
+  const reportModerationEvent = async (payload: {
+    targetedPostUri: string;
+    reason: ReasonType;
+    toServices: { label: string; value: string }[];
+    targetedUserDid: string;
+    feedUri: string;
+    feedName: string | undefined;
+  }) => {
+    try {
+      await fetch('/api/permissions/mod-event', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+    } catch (error) {
+      console.error('Error logging moderation event:', error);
+      throw error;
+    }
+  };
+
+  const handleReportPost = async () => {
+    if (
+      !reportData.moderatedPostUri ||
+      !reportData.reason ||
+      feed.length === 0
+    ) {
+      toast({
+        title: 'Error',
+        message: 'No Post Selected.',
+        intent: VisualIntent.Error,
+      });
+      return;
+    }
+
+    try {
+      const postToModerate = feed.find(
+        (post) => post.post.uri === reportData.moderatedPostUri
+      );
+      if (!postToModerate) {
+        toast({
+          title: 'Error',
+          message: 'No Post Selected.',
+          intent: VisualIntent.Error,
+        });
+        return;
+      }
+      setIsReportSubmitting(true);
+
+      const payload = {
+        targetedPostUri: reportData.moderatedPostUri,
+        reason: reportData.reason.reason,
+        toServices: reportData.toServices,
+        targetedUserDid: postToModerate.post.author.did,
+        feedUri,
+        feedName: feedName || 'Unnamed Feed',
+      };
+
+      await reportModerationEvent(payload);
+
+      closeModalInstance(MODAL_INSTANCE_IDS.REPORT_POST);
+      closeModalInstance(MODAL_INSTANCE_IDS.MOD_MENU);
+
+      setReportData((prev) => ({
+        ...prev,
+        moderatedPostUri: null,
+        toServices: [MODERATION_SERVICES[0]],
+      }));
+      toast({
+        title: 'Success',
+        message: 'Post reported successfully',
+        intent: VisualIntent.Success,
+      });
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: 'Error',
+        message: 'Unable to report post. Please try again later.',
+        intent: VisualIntent.Error,
+      });
+    } finally {
+      setIsReportSubmitting(false);
+    }
+  };
+
+  const isModServiceChecked = (data: ModerationService) =>
+    reportData.toServices.some((item) => item.value === data.value);
+
+  const handleReportToChange = (updatedReportToData: ModerationService) => {
+    if (isModServiceChecked(updatedReportToData)) {
+      setReportData((prev) => ({
+        ...prev,
+        toServices: prev.toServices.filter(
+          (item) => item.value !== updatedReportToData.value
+        ),
+      }));
+    } else {
+      setReportData((prev) => ({
+        ...prev,
+        toServices: [...prev.toServices, updatedReportToData],
+      }));
+    }
+  };
+
+  const onClose = () =>
+    setReportData((prev) => ({
+      ...prev,
+      moderatedPostUri: null,
+      toServices: [MODERATION_SERVICES[0]],
+    }));
+
+  return {
+    reportData,
+    isReportSubmitting,
+    handleModAction,
+    handleSelectReportReason,
+    handleReportPost,
+    handleReportToChange,
+    isModServiceChecked,
+    onClose,
+  };
+}
