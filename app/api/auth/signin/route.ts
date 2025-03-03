91519fd2a3087717ff914f0761bd8f18b1e2d62c
Natalie Davis <natalienetworks@gmail.com>
2025-03-02 18:26:59 -0600

    more force dynamics

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -1,4 +1,4 @@
-// app/api/auth/signin/route.ts
+export const dynamic = 'force-dynamic';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
--- app/oauth/login/page.tsx
+++ app/oauth/login/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+export const dynamic = 'force-dynamic';
+
 import { Login } from '@/components/login';
 
 export default async function page() {
