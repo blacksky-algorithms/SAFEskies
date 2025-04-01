42448bb27a240915203acaf25718a2499c38615b
Natalie Davis <natalienetworks@gmail.com>
2025-04-01 14:46:04 -0500

    typo in default feed title

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -6,6 +6,6 @@ export const preferredLanguages = 'en-US, en';
 
 export const DEFAULT_FEED = {
   uri: 'at://did:plc:w4xbfzo7kqfes5zb7r6qv3rw/app.bsky.feed.generator/blacksky',
-  displayName: 'Blacksy',
+  displayName: 'Blacksky',
   type: 'user' as UserRole,
 } as const;
